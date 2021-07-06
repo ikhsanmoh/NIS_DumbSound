c988c88d23d189f482561248b08e6bdc03ad595c
Ikhsan Mohamad <icanmohamad13@gmail.com>
2021-07-06 18:47:04 +0700

    Add Register

--- client/src/components/modal/RegistrationModal.js
+++ client/src/components/modal/RegistrationModal.js
@@ -1,48 +1,58 @@
 import { useState } from 'react'
-// import { API, setAuthToken } from '../../config/api'
+import { API, setAuthToken } from '../../config/api'
 import Modal from './Modal'
 import Button from '../button/Button';
 
 import './FormsModal.css'
 
 const RegistrationModal = ({ switchModal, modalStat, modalClose }) => {
-  const [email, setEmail] = useState('')
   const [name, setName] = useState('')
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [listAs, setListAs] = useState('')
+  const [gender, setGender] = useState('')
+  const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('')
 
   const onSubmitHandler = async (e) => {
-    // try {
-    //   e.preventDefault()
+    try {
+      e.preventDefault()
 
-    //   const config = {
-    //     headers: {
-    //       "Content-type": "application/json"
-    //     }
-    //   }
+      const config = {
+        headers: {
+          "Content-type": "application/json"
+        }
+      }
 
-    //   const body = JSON.stringify({
-    //     email,
-    //     username,
-    //     password,
-    //     fullName: name
-    //   })
+      const body = JSON.stringify({
+        fullName: name,
+        email,
+        password,
+        listAs: '0',
+        gender,
+        phone,
+        address
+      })
 
-    //   const response = await API.post("/register", body, config)
+      const response = await API.post("/register", body, config)
 
-    //   if (response.status === 200) {
-    //     alert('Registration Success!')
-    //   }
+      if (response.status === 200) {
+        alert('Registration Success!')
+      }
 
-    //   setEmail('')
-    //   setName('')
-    //   setUsername('')
-    //   setPassword('')
+      setName('')
+      setEmail('')
+      setPassword('')
+      setAddress('')
+      setListAs('')
+      setGender('')
+      setPhone('')
+      setAddress('')
 
-    //   modalClose()
-    // } catch (error) {
-    //   alert(error?.response?.data?.message)
-    // }
+      modalClose()
+    } catch (error) {
+      alert(error?.response?.data?.message)
+    }
   }
 
   return (
@@ -60,27 +70,41 @@ const RegistrationModal = ({ switchModal, modalStat, modalClose }) => {
               required
             />
             <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={e => setName(e.target.value)}
-              required
-            />
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              required
-            />
-            <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={e => setPassword(e.target.value)}
               required
             />
-            <Button text="Register" className="btn" />
+            <input
+              type="text"
+              placeholder="Full Name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              required
+            />
+            <select
+              onChange={e => setGender(e.target.value)}
+            >
+              <option value="">Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Phone"
+              value={phone}
+              onChange={e => setPhone(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Address"
+              value={address}
+              onChange={e => setAddress(e.target.value)}
+              required
+            />
+            <Button text="Register" className="btn" onClick={false} />
           </form>
           <p>
             Already have an account ? Klik
