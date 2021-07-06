32ff159ba5294cb16e2e6344f32777535a4a56a7
Ikhsan Mohamad <icanmohamad13@gmail.com>
2021-07-06 12:21:28 +0700

    Init server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const routers = require('./src/routers')
+// const cors = require('cors');
+
+const app = express()
+
+// use Body Parser
+app.use(express.json())
+// app.use(cors())
+
+app.use('/api/v1', routers)
+// app.use('/uploads', express.static('uploads'))
+
+app.listen(process.env.port || 5000, () => {
+  console.log('Server Listening...')
+})
