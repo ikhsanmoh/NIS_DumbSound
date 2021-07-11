50eabc161f98354c9d1370efaabce80db9064599
Ikhsan Mohamad <icanmohamad13@gmail.com>
2021-07-11 11:03:27 +0700

    Tidy up routers code

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -2,41 +2,46 @@ require('dotenv').config()
 const express = require('express')
 const router = express.Router()
 
-// Controllers
+// Auth Controllers
 const {
   login,
   registration
 } = require('../controllers/auth')
 
+// Music Controllers
 const {
   addArtist,
   addMusic,
   getMusics
 } = require('../controllers/music')
 
+// Payment/Transaction Controllers
 const {
   addPayment,
   getPayments,
   changePaymentStatus
 } = require('../controllers/payment')
 
-// Middleware
+// Middlewares
 const { auth } = require('../middleware/auth')
 const { uploadFile } = require('../middleware/uploadFile')
 
-// Constant
+// Constants
 const UPLOADS_IMG_FIELD_NAME = 'image'
 const UPLOADS_AUDIO_FIELD_NAME = 'audio'
 
-// Endpoints
+// Auth Endpoints
 router.post('/login', login)
 router.post('/register', registration)
-router.get('/musics', getMusics)
+
+// Music Endpoints
 router.post('/artist', auth, addArtist)
+router.get('/musics', getMusics)
+router.post('/music', auth, uploadFile(UPLOADS_IMG_FIELD_NAME, UPLOADS_AUDIO_FIELD_NAME), addMusic)
+
+// Payment/Transaction Endpoints
 router.post('/transaction', auth, uploadFile(UPLOADS_IMG_FIELD_NAME), addPayment)
 router.get('/transactions', auth, getPayments)
 router.put('/transaction', auth, changePaymentStatus)
-router.post('/music', auth, uploadFile(UPLOADS_IMG_FIELD_NAME, UPLOADS_AUDIO_FIELD_NAME), addMusic)
-
 
 module.exports = router
