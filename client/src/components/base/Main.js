ff96b6bf754ec283a6f516f7b49a93243da1daa4
Ikhsan Mohamad <icanmohamad13@gmail.com>
2021-07-21 23:16:07 +0700

    Add search music

--- client/src/components/base/Main.js
+++ client/src/components/base/Main.js
@@ -1,17 +1,47 @@
+import { useState } from 'react';
+
 import SongCard from '../card/SongCard';
 
 import './Main.css';
 
+const inputSearchStyle = {
+  backgroundImage: `url(/search-icon.png)`
+}
+
 const Main = ({ openMusicPlayer, musics }) => {
-  // const musicsHasItems = musics.length
+  const [search, setSearch] = useState('')
+  const [musicList, setMusicList] = useState(musics)
+
+  const searchHandler = (e) => {
+    e.preventDefault()
+
+    const filterMusics = musics.filter(music => {
+      const titleLowerCase = music.title.toLowerCase()
+      const searchLowerCase = search.toLowerCase()
+      return titleLowerCase.includes(searchLowerCase)
+    })
+
+    setMusicList(filterMusics)
+  }
 
   return (
     <main className="main">
       <div className="main-top">
         <h4>Dengarkan Dan Rasakan</h4>
       </div>
+      <div className="search">
+        <form onKeyUp={searchHandler}>
+          <input
+            style={inputSearchStyle}
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+        </form>
+      </div>
       <div className="main-body">
-        {musics && musics.map((music, index) => (
+        {musicList && musicList.map((music, index) => (
           <SongCard
             key={index}
             image={music.thumbnail}
